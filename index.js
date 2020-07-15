2ece26ce4d8514be631cb2b664be081f0035b533
Alena Zhmurko <alenazhmurko@gmail.com>
2020-07-15 15:34:27 +0300

    Add some handlers

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Telegraf = require('telegraf');
 const axios = require('axios');
-const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN || "your_token";
+const cc = require('currency-codes');
+
+const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN || "yout_token";
 
 const bot = new Telegraf(TELEGRAM_BOT_TOKEN);
 
@@ -8,15 +10,30 @@ bot.start(ctx => {
   return ctx.reply("Welcome to Mono Currency Bot");
 });
 //bot handler
-bot.hears('hi', async (ctx)=> {
+bot.hears(/^[A-Z]+$/i, async (ctx)=> {
+  const clientCurCore = ctx.message.text;
+  const currency = cc.code(clientCurCore);
+// check for existing currency
+  if (!currency){
+    return ctx.reply('Currency didnt found');
+  }
+
   try {
     const currencyObj = await axios.get(
       'https://api.monobank.ua/bank/currency'
       );
-    return ctx.reply(currencyObj.data[0]);
 
+    const foundCurrency = currencyObj.data.find((cur) => {
+      return cur.currencyCodeA.toString() === currency.number;
+    });
+
+    if (!foundCurrency){
+      return ctx.reply('Currency didnt found in Monobank API');
+    }
+    return ctx.reply(foundCurrency);
   } catch (error) {
     return ctx.reply(error);
   }
 });
+
 bot.startPolling();
